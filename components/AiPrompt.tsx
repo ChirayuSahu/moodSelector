b35f4f36e5119a208e016a60d4d1387aa56dcd2e
Chirayu Sahu <81841434+ChirayuSahu@users.noreply.github.com>
2025-03-18 10:14:17 +0530

    Update AiPrompt.tsx

--- components/AiPrompt.tsx
+++ components/AiPrompt.tsx
@@ -30,7 +30,7 @@ export default function AiPrompt() {
 
     try {
       const genAI = new GoogleGenerativeAI(apiKey);
-      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+      const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
       const result = await model.generateContent(newPrompt);
       let responseText = await result.response.text();
